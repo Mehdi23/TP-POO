5c077ee7b6ecc9837ae56b0b2009c3b100e506be
malaoui <malaoui@PC-MALAOUI.m2mgroup.com>
2015-04-03 17:01:25 +0000

    new

---
+++ POO-TP/src/org/mehdi/project/TpPoo.java
@@ -0,0 +1,16 @@
+package org.mehdi.project;
+
+public class TpPoo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//03-04-2015 TP0
+		System.out.print("Hello World !");
+
+	}
+
+}
