7bf153473f9a71f50f4e61156290f03f681c1d87
mehdi <mehdi@DESKTOP-A7MKDCV.Home>
2016-02-06 23:05:58 +0000

    new

--- my-app/src/main/webapp/js/index.js
+++ my-app/src/main/webapp/js/index.js
@@ -1,14 +1,15 @@
-/*var rootURL = "http://localhost:8080/rest"*/
-var rootURL = "http://mehdiappk.appspot.com/rest"
+var rootURL = "http://localhost:8080/rest"
+//var rootURL = "http://mehdiappk.appspot.com/rest"
+var datyp = "json"
 var index = 0
 
 $( ".treeview-menu-entity" ).click(function() {
 
-     $("#content-header-text").html("Référenciel <span ><small>"+$(this).attr('name')+"</small></span>");         
+     $("#content-header-text").html("Référenciel");         
      $("#page-header").load("pages/crud.html");
      $("#page-content" ).load( "entite/"+$(this).attr('name')+".html" );
-     $("#page-footer").load("pages/paging.html");
      $("#entity" ).text($(this).attr('name'));
+     $( ".sidebar-toggle" ).trigger( "click" );
 })
 
 $(document.body).on('click', '#create' ,function(){
@@ -17,10 +18,12 @@ $(document.body).on('click', '#create' ,function(){
 		type : 'POST',
 		url : rootURL + '/' + $('#entity').text(),
 		contentType: 'application/json',
-		dataType : "jsonp",
+		dataType : datyp,
 		data : $('#entityform').serializeJSON(),
 		success : function(data) {
 			alert('Saved ...');
+			$('#entityform')[0].reset();
+			$( "#search" ).trigger( "click" );
 		},
 		error : function(jqXHR, textStatus, errorThrown) {
 			alert('Error : ' + textStatus);
@@ -35,8 +38,10 @@ $(document.body).on('click', '#search' ,function(){
   $.ajax({
 		type : 'GET',
 		url : rootURL + '/' + $('#entity').text(),
-		dataType : "json",
+		dataType : datyp,
 		success : function(data) {
+		    $(".btn-crud").prop('disabled', true);
+		    if(data.length>1) $(".btn-crud").prop('disabled', false);   
 		    $("#current").text(1);
 		    $("#total").text(data.length);
 			$('#entityform').loadJSON(data[index]);
@@ -53,11 +58,12 @@ $(document.body).on('click', '#update' ,function(){
         type: 'PUT',
         contentType: 'application/json',
         url: rootURL + '/' + $('#entity').text(),
-        dataType: "json",
+        dataType: datyp,
         data: $('#entityform').serializeJSON(),
         success: function(textStatus, jqXHR){
             alert('updated successfully');
-            location.reload();
+            $('#entityform')[0].reset();
+			$( "#search" ).trigger( "click" );
         },
         error: function(jqXHR, textStatus, errorThrown){
             alert('update error: ' + textStatus);
@@ -70,11 +76,11 @@ $(document.body).on('click', '#delete' ,function(){
     $.ajax({
         type: 'DELETE',
         contentType: 'application/json',
-		dataType : "json",
+		dataType : datyp,
         url: rootURL + '/' + $('#entity').text() + '/' + $("#id").val(),
         success: function(textStatus, jqXHR){
             alert('deleted successfully ');
-            location.reload();
+            $( "#search" ).trigger( "click" );
         },
         error: function(jqXHR, textStatus, errorThrown){
             alert('delete error ');
@@ -89,7 +95,7 @@ $(document.body).on('click', '#next' ,function(){
   $.ajax({
 		type : 'GET',
 		url : rootURL + '/' + $('#entity').text(),
-		dataType : "json",
+		dataType : datyp,
 		success : function(data) {
 		    $("#current").text(index);
 		    $("#total").text(data.length);
@@ -108,7 +114,7 @@ $(document.body).on('click', '#prev' ,function(){
   $.ajax({
 		type : 'GET',
 		url : rootURL + '/' + $('#entity').text(),
-		dataType : "json",
+		dataType : datyp,
 		success : function(data) {
 		    $("#current").text(index);
 		    $("#total").text(data.length);
@@ -126,7 +132,7 @@ $(document.body).on('click', '#first' ,function(){
   $.ajax({
 		type : 'GET',
 		url : rootURL + '/' + $('#entity').text(),
-		dataType : "json",
+		dataType : datyp,
 		success : function(data) {
 		    $("#current").text(1);
 		    $("#total").text(data.length);
@@ -143,7 +149,7 @@ $(document.body).on('click', '#last' ,function(){
   $.ajax({
 		type : 'GET',
 		url : rootURL + '/' + $('#entity').text(),
-		dataType : "json",
+		dataType : datyp,
 		success : function(data) {
 		    $("#current").text(data.length);
 		    $("#total").text(data.length);
